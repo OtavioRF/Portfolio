69d81f429ebdde5130202a209103c00cb64ca4ad
OtavioRF <otavio.felippi@estudante.ifms.edu.br>
2023-08-29 08:23:16 -0400

    Alterações main e header

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -5,16 +5,27 @@ export default function Header(){
      <header>
        
   <nav className="navbar">
+
   <img src={teste} alt="logo" />
+
+
    <ul className="botoes">
      
       <li><a href="#">Inicio</a></li>
       <li><a href="#">Projetos</a></li>
       <li><a href="#">Sobre</a></li>
-      <li><a href="#">Contato</a></li>
             
   </ul>
+  
+ <div className='button_Contato'>
+
+          <a href='#'><button>Contato</button></a>
+
+      </div>
+
       </nav>
+     
+
     </header>
   )
 };
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,9 +1,32 @@
 import './Main.css'
+import pc from "../../assets/pc_pixel.png"
 export default function Main(){
   return(
   <main>
 
-    
+    <section className='topo_site'>
+      <div className='interface'>
+        <div className='flex'>
+          <div className='txt_site'>
+            <h1>TRANSFORMANDO IDEIAS EM CÓDIGO, UM PROJETO POR VEZ.</h1>
+            <p>meu portfólio reflete a jornada de alguém que se dedica a transformar desafios em oportunidades através do mundo dinâmico do desenvolvimento de software. Cada linha de 
+              código é um testemunho do meu compromisso em traduzir conceitos em aplicações tangíveis, enquanto continuo 
+              explorando novas tecnologias e aprimorando minhas habilidades para criar experiências digitais inovadoras e impactantes.</p>
+
+              <div className='botao_contato'>
+                <a href='#'><button>Entrar em contato</button></a>
+              </div>
+          </div>
+
+          <div className='img_top'>
+          <img src={pc} alt="logo" />
+
+          </div>
+        </div>
+    </div>
+
+    </section>
+
   </main>
   )
 };
